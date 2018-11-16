09ead00f74262377f7aa431b7e28ab769d3e0203
Zach Selchau <zhselchau1998@yahoo.com>
2018-11-16 15:45:08 -0800

    Finished prog3_2.lua. Testing done in repl.it and copied over

--- prog3_2.lua
+++ prog3_2.lua
@@ -4,15 +4,17 @@ function infixToPostfix(str)
 
     local tokens = {} --postfix tokens
     local stack = {} --stack for operators
+    stack[0] = "e"
     local topOfStack=0;
     
-    local prec = {
-        ^ = 3,
-        * = 2,
-        / = 2,
-        + = 1,
-        - = 1
-    }
+    local prec = {}
+    prec['^'] = 3
+    prec['*'] = 2
+    prec['/'] = 2
+    prec['+'] = 1
+    prec['-'] = 1
+    prec['e'] = 0
+    prec['('] = -1
 
     for i in string.gmatch(str, "%S+") do
         
@@ -20,21 +22,61 @@ function infixToPostfix(str)
             topOfStack = topOfStack + 1
             table.insert(stack, topOfStack, i)
         elseif i == ")" then --start popping stuff off stack until ( is reached
-            
-        elseif i == "^" then --pop off everything that has less precedent
-            
-        elseif i == "*" then --pop off everything that has less precedent
-            
-        elseif i == "/" then --pop off everything that has less precedent
-            
-        elseif i == "+" then --pop off everything that has less precedent
-            
-        elseif i == "-" then --pop off everything that has less precedent
-            
+            while (stack[topOfStack] ~= "(" do
+                table.insert(tokens, stack[topOfStack])
+                table.remove(stack)
+                topOfStack = topOfStack - 1
+            end
+            table.remove(stack)
+        elseif i == "^" then --pop off everything that has more or == precedent
+            while (prec[stack[topOfStack]] >= prec [i]) do
+                table.insert(tokens, stack[topOfStack])
+                topOfStack = topOfStack - 1
+                table.remove(stack)
+            end
+            topOfStack = topOfStack + 1
+            table.insert(stack, topOfStack, i)
+        elseif i == "*" then --pop off everything that has more or == precedent
+            while (prec[stack[topOfStack]] >= prec [i]) do
+                table.insert(tokens, stack[topOfStack])
+                topOfStack = topOfStack - 1
+                table.remove(stack)
+            end
+            topOfStack = topOfStack + 1
+            table.insert(stack, topOfStack, i)
+        elseif i == "/" then --pop off everything that has more or == precedent
+            while (prec[stack[topOfStack]] >= prec [i]) do
+                table.insert(tokens, stack[topOfStack])
+                topOfStack = topOfStack - 1
+                table.remove(stack)
+            end
+            topOfStack = topOfStack + 1
+            table.insert(stack, topOfStack, i)
+        elseif i == "+" then --pop off everything that has more or == precedent
+            while (prec[stack[topOfStack]] >= prec [i]) do
+                table.insert(tokens, stack[topOfStack])
+                topOfStack = topOfStack - 1
+                table.remove(stack)
+            end
+            topOfStack = topOfStack + 1
+            table.insert(stack, topOfStack, i)
+        elseif i == "-" then --pop off everything that has more or == precedent
+            while (prec[stack[topOfStack]] >= prec [i]) do
+                table.insert(tokens, stack[topOfStack])
+                topOfStack = topOfStack - 1
+                table.remove(stack)
+            end
+            topOfStack = topOfStack + 1
+            table.insert(stack, topOfStack, i)
         else
             table.insert(tokens, i) --always put in tokens
         end
         
+    end
+
+    while(prec[stack[topOfStack]] ~= 0) do
+        table.insert(tokens, stack[topOfStack])
+        topOfStack = topOfStack - 1
     end
 
     return table.concat(tokens, " ")
