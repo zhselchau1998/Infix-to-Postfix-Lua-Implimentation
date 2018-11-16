64a087f90cc382581707e8aadca353a09fbcc36f
Zach Selchau <zhselchau1998@yahoo.com>
2018-11-15 17:28:29 -0800

    setup the general format for prog3_2.lua

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,1 +1,27 @@
-print("Hello World!")
+function infixToPostfix(str)
+
+    local tokens = {}
+    local stack = {}
+
+    for i in string.gmatch(str, "%S+") do
+        
+        if i == "(" then
+            
+        elseif i == ")" then
+            
+        elseif i == "^" then
+            
+        elseif i == "*" then
+            
+        elseif i == "/" then
+            
+        elseif i == "+" then
+            
+        elseif i == "-" then
+            
+        else
+            
+        end
+        
+    end
+end
