599346f02f9658f5bcda556df1329bf203d1f44f
Zach Selchau <zhselchau1998@yahoo.com>
2018-11-15 18:03:45 -0800

    Added a precedent table and started on the conditionals

--- prog3_2.lua
+++ prog3_2.lua
@@ -1,27 +1,39 @@
 function infixToPostfix(str)
 
-    local tokens = {}
-    local stack = {}
+    local tokens = {} --postfix tokens
+    local stack = {} --stack for operators
+    local topOfStack=0;
+    
+    local prec = {
+        ^ = 3,
+        * = 2,
+        / = 2,
+        + = 1,
+        - = 1
+    }
 
     for i in string.gmatch(str, "%S+") do
         
-        if i == "(" then
+        if i == "(" then --always put on stack
+            topOfStack = topOfStack + 1
+            table.insert(stack, topOfStack, i)
+        elseif i == ")" then --start popping stuff off stack until ( is reached
             
-        elseif i == ")" then
+        elseif i == "^" then --pop off everything that has less precedent
             
-        elseif i == "^" then
+        elseif i == "*" then --pop off everything that has less precedent
             
-        elseif i == "*" then
+        elseif i == "/" then --pop off everything that has less precedent
             
-        elseif i == "/" then
+        elseif i == "+" then --pop off everything that has less precedent
             
-        elseif i == "+" then
-            
-        elseif i == "-" then
+        elseif i == "-" then --pop off everything that has less precedent
             
         else
-            
+            table.insert(tokens, i) --always put in tokens
         end
         
     end
+
+    return table.concat(tokens, "")
 end
